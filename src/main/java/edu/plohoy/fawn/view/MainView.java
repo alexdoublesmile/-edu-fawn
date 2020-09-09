032596771eef45c54c7f04be98ad968360c270a8
Alex Plohoy <alexplohoy@mail.ru>
2020-09-09 17:23:51 +0300

    fix spring boot & vaadin versions

--- src/main/java/edu/plohoy/fawn/view/MainView.java
+++ src/main/java/edu/plohoy/fawn/view/MainView.java
@@ -1,8 +1,6 @@
 package edu.plohoy.fawn.view;
 
-import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.Route;
+
 import edu.plohoy.fawn.dao.EmployeeDao;
 import edu.plohoy.fawn.domain.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
