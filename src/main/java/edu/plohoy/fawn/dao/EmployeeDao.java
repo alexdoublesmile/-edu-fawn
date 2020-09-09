79f021e8259565cd323916379426a457d5eb0e16
Alex Plohoy <alexplohoy@mail.ru>
2020-09-09 15:56:10 +0300

    add to EmployeeDao: findByName()

--- src/main/java/edu/plohoy/fawn/dao/EmployeeDao.java
+++ src/main/java/edu/plohoy/fawn/dao/EmployeeDao.java
@@ -2,6 +2,15 @@ package edu.plohoy.fawn.dao;
 
 import edu.plohoy.fawn.domain.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface EmployeeDao extends JpaRepository<Employee, Long> {
+
+    @Query("from Employee e " +
+            "where concat(e.lastName, ' ', e.firstName, ' ', e.patronymic) " +
+            "like concat('%', :name, '%')")
+    List<Employee> getByName(@Param("name") String name);
 }
