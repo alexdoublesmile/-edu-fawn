8a08065e85519ec2eea8aa5b26458b783d681d59
Alex Plohoy <alexplohoy@mail.ru>
2020-09-09 17:42:33 +0300

    add component.EmployeeEditor

---
+++ src/main/java/edu/plohoy/fawn/component/EployeeEditor.java
@@ -0,0 +1,7 @@
+package edu.plohoy.fawn.component;
+
+import com.vaadin.flow.component.KeyNotifier;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+public class EployeeEditor extends VerticalLayout implements KeyNotifier {
+}
