1169068a44aa382e2fa17e65388a2100eda98cd9
Alex Plohoy <alexplohoy@mail.ru>
2020-09-09 16:02:32 +0300

    add view.MainView

---
+++ src/main/java/edu/plohoy/fawn/view/MainView.java
@@ -0,0 +1,8 @@
+package edu.plohoy.fawn.view;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route
+public class MainView extends VerticalLayout {
+}
