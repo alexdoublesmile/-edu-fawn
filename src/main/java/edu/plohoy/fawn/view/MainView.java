4b6f1df0e24363c6d29f6044ff7bd6d261146216
Alex Plohoy <alexplohoy@mail.ru>
2020-09-09 16:06:42 +0300

    add to MainView: dao & grid

--- src/main/java/edu/plohoy/fawn/view/MainView.java
+++ src/main/java/edu/plohoy/fawn/view/MainView.java
@@ -1,8 +1,23 @@
 package edu.plohoy.fawn.view;
 
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import edu.plohoy.fawn.dao.EmployeeDao;
+import edu.plohoy.fawn.domain.Employee;
 
 @Route
 public class MainView extends VerticalLayout {
+
+    private EmployeeDao dao;
+    private Grid<Employee> grid;
+
+    public MainView(EmployeeDao dao) {
+        this.dao = dao;
+
+        grid = new Grid<>();
+        add(grid);
+
+        grid.setItems(dao.findAll());
+    }
 }
