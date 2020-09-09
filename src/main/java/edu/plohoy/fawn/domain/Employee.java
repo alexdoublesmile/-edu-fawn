c42f27f0bf9cda5be17503a3c91b7b19b634022b
Alex Plohoy <alexplohoy@mail.ru>
2020-09-09 15:46:09 +0300

    add domain.Eployee

---
+++ src/main/java/edu/plohoy/fawn/domain/Employee.java
@@ -0,0 +1,18 @@
+package edu.plohoy.fawn.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Employee {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String patronymic;
+}
