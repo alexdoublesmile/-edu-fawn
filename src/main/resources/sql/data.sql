455e505277dd841bdfd592224b3bf4f3c6567b11
Alex Plohoy <alexplohoy@mail.ru>
2020-09-09 16:10:35 +0300

    add resources/sql/data.sql

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,5 @@
+insert into fawn(id, first_name, last_name, patronymic) values
+  (1, 'Ivan', 'Ivanovich', 'Ivanov'),
+  (2, 'Peter', 'Petrovich', 'Petrov'),
+  (3, 'Sidor', 'Sidorovich', 'Sidoroov'),
+  (4, 'Alex', 'Smith', '<none>');
